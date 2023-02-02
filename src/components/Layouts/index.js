ae89ddfbea0d4c2a402342d6e55b7c801540369a
louis7shakya <louis.shakya@huskers.unl.edu>
2023-02-02 05:26:25 -0600

    cleaned up code

--- src/components/Layouts/index.js
+++ src/components/Layouts/index.js
@@ -1,25 +1,17 @@
 import React, { useState } from "react";
-import { Layout, Row, Col, Input, Typography, Select } from "antd";
+import { Layout, Row, Col, Input, Typography } from "antd";
 import "./layout.css";
 import Weather from "../weather";
 
 const { Header, Content } = Layout;
 const { Search } = Input;
 const { Title } = Typography;
-const { Option } = Select;
-
-const daysList = [{ value: 3 }, { value: 5 }, { value: 7 }, { value: 14 }];
 
 const DesignLayout = () => {
   const [searchValue, setSearchValue] = useState("kathmandu");
-  const [days, setDays] = useState(7);
 
   const onSearch = (value) => {
     setSearchValue(value);
-  };
-
-  const handleDayChange = (value) => {
-    setDays(value);
   };
 
   return (
@@ -29,31 +21,12 @@ const DesignLayout = () => {
           <Col span={12} style={{ marginTop: "6px" }}>
             <Title style={{ color: "white" }}>Weather App</Title>
           </Col>
-          <Col span={6}>
-            <Row style={{ marginTop: "15px" }}>
-              <Title
-                value={3}
-                style={{ color: "white", margin: 0, lineHeight: 0.8 }}
-              >
-                Display Days
-              </Title>
-              <Select
-                onChange={handleDayChange}
-                style={{
-                  width: "100px",
-                  marginRight: "20px",
-                  marginLeft: "20px",
-                }}
-              >
-                {daysList.map((day) => (
-                  <Option value={day?.value} key={day?.value}>
-                    {day.value}
-                  </Option>
-                ))}
-              </Select>
-            </Row>
-          </Col>
-          <Col span={6} style={{ marginTop: "15px" }}>
+          <Col
+            span={12}
+            style={{
+              marginTop: "15px",
+            }}
+          >
             <Search
               placeholder="input search text"
               onSearch={onSearch}
@@ -70,7 +43,7 @@ const DesignLayout = () => {
           className="site-layout-background"
           style={{ padding: 24, minHeight: 910 }}
         >
-          <Weather searchValue={searchValue} days={days} />
+          <Weather searchValue={searchValue} />
         </div>
       </Content>
     </Layout>
