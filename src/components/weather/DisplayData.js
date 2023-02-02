021bc8a71d595c8010e5654e3847962447e46360
louis7shakya <louis.shakya@huskers.unl.edu>
2023-02-02 05:27:24 -0600

    changed the layout and added section to display hourly weather data

--- src/components/weather/DisplayData.js
+++ src/components/weather/DisplayData.js
@@ -1,85 +1,156 @@
 import { Row, Col, Card } from "antd";
+import { ICON_MAP } from "./iconMap";
 
-const DisplayData = ({ weatherData }) => {
-  const currentDate = new Date();
+const dayFormatter = new Intl.DateTimeFormat(undefined, { weekday: "long" });
+const hourFormatter = new Intl.DateTimeFormat(undefined, { hour: "numeric" });
+
+const getIconUrl = (iconCode) => {
+  return `icons/${ICON_MAP.get(iconCode)}.svg`;
+};
+
+const DisplayData = ({ weatherData, address }) => {
+  const { current, daily, hourly } = weatherData;
+
   return (
     <>
-      <Row>
-        <Col span={8}>
-          <Row style={{ margin: "50px" }}>
-            <Col span={9}>
-              <img
-                src={`http://openweathermap.org/img/wn/${
-                  weatherData?.data &&
-                  weatherData?.data[0]?.weather?.icon.substring(1)
-                }@2x.png`}
-                alt=""
-              />
-            </Col>
-            <Col span={15}>
-              <p>
-                City: <strong>{weatherData?.city_name}</strong>
-              </p>
-              <p>
-                TimeZone: <strong>{weatherData?.timezone}</strong>
-              </p>
-              <p>
-                Weather:{" "}
-                <strong>
-                  {weatherData?.data &&
-                    weatherData?.data[0]?.weather?.description}
-                </strong>
-              </p>
-              <p>
-                Wind Speed:{" "}
-                <strong>
-                  {weatherData?.data && weatherData?.data[0]?.wind_spd}{" "}
-                  {"km/hr"}
-                </strong>
-              </p>
-              <p>
-                Date: <strong>{currentDate.toLocaleString()}</strong>
-              </p>
-            </Col>
-          </Row>
-        </Col>
-        <Col span={16}>
-          {weatherData?.data &&
-            weatherData?.data?.length &&
-            weatherData?.data.map((currentData, index) => {
-              if (index !== 0) {
-                return (
+      <Col>
+        <Row style={{ margin: "50px" }}>
+          <Col
+            span={12}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <img
+              style={{ width: "80px", height: "80px" }}
+              src={getIconUrl(current?.iconCode)}
+              alt={getIconUrl(current?.iconCode)}
+            />
+            <span style={{ fontSize: "3rem" }}>
+              {current?.currentTemp}&deg;
+            </span>
+          </Col>
+          <Col span={12} style={{ fontSize: "1rem" }}>
+            <p>
+              City: <strong>{address?.city}</strong>
+            </p>
+            <p>
+              High: <strong>{current?.highTemp}&deg;</strong>
+            </p>
+            <p>
+              Low: <strong>{current?.lowTemp}&deg;</strong>
+            </p>
+            <p>
+              Wind Speed:{" "}
+              <strong>
+                {current?.windSpeed} {"mph"}
+              </strong>
+            </p>
+            <p>
+              Precipitaion: <strong>{current?.precip}</strong>
+            </p>
+          </Col>
+        </Row>
+      </Col>
+      <Col
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {daily &&
+          daily?.map((day, index) => {
+            if (index !== 0) {
+              return (
+                <div style={{ margin: 10, padding: 10 }} key={index}>
+                  <Card style={{ textAlign: "center" }}>
+                    <img
+                      src={getIconUrl(day?.iconCode)}
+                      alt={getIconUrl(day?.iconCode)}
+                    />
+                    <p>
+                      <strong>{dayFormatter.format(day?.timestamp)}</strong>
+                    </p>
+                    <p>
+                      <strong>{day?.maxtemp}&deg;</strong>
+                    </p>
+                  </Card>
+                </div>
+              );
+            }
+          })}
+      </Col>
+      <Col
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        {hourly &&
+          hourly?.map((hour, index) => {
+            if (index !== 0) {
+              return (
+                <Card
+                  key={index}
+                  style={{
+                    width: "100%",
+                    textAlign: "center",
+                  }}
+                >
                   <div
-                    style={{ margin: 10, padding: 10 }}
-                    key={currentData?.datetime}
+                    style={{ display: "flex", justifyContent: "space-around" }}
                   >
-                    <Card style={{ textAlign: "center" }}>
-                      <img
-                        src={`http://openweathermap.org/img/wn/${currentData?.weather?.icon.substring(
-                          1
-                        )}@2x.png`}
-                        alt=""
-                      />
+                    <div>
                       <p>
-                        Date: <strong>{currentData?.datetime}</strong>
+                        <strong>{dayFormatter.format(hour.timestamp)}</strong>
                       </p>
+
                       <p>
-                        Weather:{" "}
-                        <strong>{currentData?.weather?.description}</strong>
+                        <strong>{hourFormatter.format(hour?.timestamp)}</strong>
                       </p>
+                    </div>
+                    <img
+                      src={getIconUrl(hour?.iconCode)}
+                      alt={""}
+                      width={100}
+                    />
+                    <div>
                       <p>
-                        Wind Speed:{" "}
-                        <strong>
-                          {currentData?.wind_spd} {"km/hr"}
-                        </strong>
+                        <strong>Temp</strong>
                       </p>
-                    </Card>
+
+                      <p>
+                        <strong>{hour?.temp}&deg;</strong>
+                      </p>
+                    </div>
+                    <div>
+                      <p>
+                        <strong>Wind</strong>
+                      </p>
+
+                      <p>
+                        <strong>{hour?.windSpeed} mph</strong>
+                      </p>
+                    </div>
+                    <div>
+                      <p>
+                        <strong>Precip</strong>
+                      </p>
+
+                      <p>
+                        <strong>{hour?.precip}</strong>
+                      </p>
+                    </div>
                   </div>
-                );
-              }
-            })}
-        </Col>
-      </Row>
+                </Card>
+              );
+            }
+          })}
+      </Col>
     </>
   );
 };
